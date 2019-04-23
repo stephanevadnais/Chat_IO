76afa4fc874bcba0d8ac21a0281d7f376deed8bb
Vladsteak <34750709+Vladsteak@users.noreply.github.com>
2019-04-23 09:32:00 -0400

    Connection et deconnection serveur_IO

--- serveur/serveur.js
+++ serveur/serveur.js
@@ -1,13 +1,26 @@
 const chemin = require('path');
+const http = require('http');
 const cheminPublique =  chemin.join(__dirname, '../publique');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const express = require('express');
+const socketIO = require('socket.io');
 var app = express();
+var serveur = http.createServer(app);
+var io = socketIO(serveur);
 
 
 app.use(express.static(cheminPublique));
+io.on('connection',(socket)=>{
+    console.log('Nouvel utilisateur Connecte');
 
-app.listen(port,()=>{
+    socket.on('disconnect',()=>{
+        console.log('Utilisateur deconnecte')
+    })
+
+
+})
+
+serveur.listen(port,()=>{
 
     console.log(`Serveur en fonction sur le port  ${port}`)
 })
